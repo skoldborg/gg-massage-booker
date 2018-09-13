5505f53c933f8d57f963598fe16dea40ba1f77ea
Pierre Sköldborg <pierre.skoldborg@creuna.se>
2018-09-13 11:24:35 +0200

    Added bundle analyzer to webpack and improved performance by only requiring relevant locales from moment

--- webpack.config.js
+++ webpack.config.js
@@ -3,13 +3,26 @@ require('babel-polyfill');
 const webpack = require('webpack');
 const HTMLWebpackPlugin = require('html-webpack-plugin');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 const path = require('path');
 
 const config = {
     entry: ['babel-polyfill', path.join(__dirname, 'client/app.js')],
+    
     output: {
         path: path.join(__dirname, 'dist'),
         filename: 'bundle.js'
+    },
+
+    // https://webpack.js.org/guides/code-splitting/
+    optimization: {
+        splitChunks: {
+            chunks: 'all'
+        }
+    },
+
+    performance: {
+        hints: false
     },
 
     module: {
@@ -53,10 +66,14 @@ const config = {
     },
 
     plugins: [
+        new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
 		new HTMLWebpackPlugin({
 			template: path.join(__dirname, 'client/index.html')
 		}),
-		new ExtractTextPlugin({ filename: '[name].css', allChunks: true })
+        new ExtractTextPlugin({ filename: '[name].css', allChunks: true }),
+        // new BundleAnalyzerPlugin({
+        //     analyzerMode: 'static'
+        // })
     ],
     
     resolve: {
