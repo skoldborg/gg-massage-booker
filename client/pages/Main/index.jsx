2d380c853b10d6fe755dc6b1570e491e1bd66710
Pierre Sköldborg <pierre.skoldborg@creuna.se>
2019-01-09 15:45:36 +0100

    Cleanup

--- client/pages/Main/index.jsx
+++ client/pages/Main/index.jsx
@@ -2,47 +2,8 @@ import 'whatwg-fetch';
 import { h, render, Component } from 'preact';
 
 import { TimeSlotsManager, Header } from '../../components';
-import requestService from '../../utils/requestService';
-import rootPath from '../../utils/rootPath';
 
 class Main extends Component {
-    // constructor() {
-    //     super();
-
-    //     this.state = {
-    //         accessToken: null,
-    //         showSignInButton: false
-    //     }
-    // }
-
-    // componentDidMount() {
-    //     this.checkForAccessToken();
-    // }
-
-    // async checkForAccessToken() {
-    //     const { token } = await requestService.getRequest(`${rootPath}/gettoken`);
-
-    //     if (token !== '' && token !== null) {
-    //         const user = await requestService.getRequest(`https://graph.microsoft.com/beta/me`, token);
-    //         const userPhotoBlob = await requestService.getRequest(`https://graph.microsoft.com/beta/me/photo/$value`, token, 'image/jpg');
-            
-    //         // if (userPhotoBlob.type === 'image/jpeg') {
-    //         //     const userPhotoUrl = URL.createObjectURL(userPhotoBlob);
-    //         //     const userPhoto = document.querySelector('img');
-    //         //     userPhoto.addEventListener('load', () => URL.revokeObjectURL(userPhotoUrl));
-
-    //         //     userPhoto.src = userPhotoUrl;
-    //         // }
-
-    //         this.setState({
-    //             accessToken: token,
-    //             user: user
-    //         })
-    //     } else {
-    //         this.setState({ showSignInButton: true })
-    //     }
-    // }
-
     render() {
         const { accessToken, user } = this.props;
 
